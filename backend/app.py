e4fadbf61f89c1392e94aa14cdc9e2964b3333ca
Troy Zada <tzada@uwaterloo.ca>
2024-01-17 13:28:56 -0500

    Updated folder structure

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from os import environ
 import time
 
-from scraper.walmart_scraper import WalmartProduct
+from src.scraper.walmart_scraper import WalmartProduct
 
 
 app = Flask(__name__)
@@ -25,11 +25,11 @@ def show_number_x10():
     number_x10 = data["number"] * 10
     return {"number": number_x10}
 
-@app.route("/api/walmart_product")
+@app.route("/api/walmart-product")
 def walmart_data():
     # Note: Was testing with product_id "711035416" (exists) and "123" (does not exist).
     product_id = request.args.get("product_id")
-    product_data = WalmartProduct().aggregate_data(product_id)
+    product_data = WalmartProduct.aggregate_data(product_id)
     return product_data
 
 if __name__ == "__main__":
