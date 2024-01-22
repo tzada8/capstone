9293a6296ee223304b8e5af22645fcf1a2797e2b
Troy Zada <tzada@uwaterloo.ca>
2024-01-22 18:17:47 -0500

    Removed temp /temp endpoint

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,16 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import "./Home.css";
 
 function Home() {
-    const [currentTime, setCurrentTime] = useState(0);
     const [currentNumber, setCurrentNumber] = useState(0);
-
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_BACKEND_BASE_API}/api/time`).then(res => res.json()).then(data => {
-            setCurrentTime(data.time);
-        });
-    }, []);
 
     const handleNumberClick = () => {
         fetch(`${process.env.REACT_APP_BACKEND_BASE_API}/api/number`, {
@@ -31,9 +24,6 @@ function Home() {
             <input type="number" onChange={handleNumberChange} />
             <button onClick={handleNumberClick}>Change Number</button>
             <p>The current number is {currentNumber}.</p>
-
-            <p>The current time is {currentTime}.</p>
-
             <p>Environment: {process.env.REACT_APP_ENVIRONMENT}.</p>
 		</div>
 	);
