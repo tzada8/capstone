152cb6b4ccbc6d30ed7f960df969ec886baa09da
Troy Zada <tzada@uwaterloo.ca>
2024-01-22 01:01:21 -0500

    Add baseline navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import Navbar from "./components/Navbar";
 import Home from "./pages/home/Home";
 import ProductSearch from "./pages/product-search/ProductSearch";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product-search" element={<ProductSearch />} />
