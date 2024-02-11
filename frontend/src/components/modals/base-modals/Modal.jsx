9a2dde3cf2806b3de2ffc195eade16efb2eca938
Troy Zada <tzada@uwaterloo.ca>
2024-02-11 16:32:35 -0500

    Added general Modal component

---
+++ frontend/src/components/modals/base-modals/Modal.jsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState, useEffect } from "react";
+
+import "./Modal.css";
+
+function Modal({ isOpen, hasCloseBtn = true, onClose, children }) {
+    const [isModalOpen, setIsModalOpen] = useState(isOpen);
+    const modalRef = useRef(null);
+
+    const handleCloseModal = () => {
+        if (onClose) onClose();
+        setIsModalOpen(false);
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Escape") handleCloseModal();
+    }
+
+    useEffect(() => setIsModalOpen(isOpen), [isOpen]);
+
+    useEffect(() => {
+        const modalElement = modalRef.current;
+        if (modalElement) {
+            if (isModalOpen) {
+                modalElement.showModal();
+            } else {
+                modalElement.close();
+            }
+        }
+    }, [isModalOpen]);
+
+	return (
+        <dialog ref={modalRef} onKeyDown={handleKeyDown} className="modal modal-center">
+            {hasCloseBtn && (
+                <button className="modal-close-btn" onClick={handleCloseModal}>
+                    Close
+                </button>
+            )}
+            {children}
+        </dialog>
+	);
+}
+
+export default Modal;
