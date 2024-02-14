5c24937df6e82d8a916fbcbbc8335fa7c45cab7f
Troy Zada <tzada@uwaterloo.ca>
2024-02-13 19:57:05 -0500

    Add token limitation

--- backend/test/summarize/test_summarize.py
+++ backend/test/summarize/test_summarize.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 from src.summarize.summarize import summarize
 
+# TODO: Impove test by confirming .create() call_args takes in bullet list of reviews.
 class TestSummarize(unittest.TestCase):
     def test_summarize_no_reviews(self):
         reviews = []
@@ -12,7 +13,6 @@ class TestSummarize(unittest.TestCase):
 
     @patch("src.summarize.summarize.OpenAI")
     def test_summarize_with_reviews(self, openai_mock):
-        # TODO: Impove test by confirming .create() call_args takes in bullet list of reviews.
         reviews = [
             "This product stinks",
             "One of the best purchases I ever made",
@@ -20,6 +20,14 @@ class TestSummarize(unittest.TestCase):
             "I'mm stll lrning to tpe, but my fvorite prodct ever",
             "4.5 stars? How is that possible. Should have a negative rating",
         ]
+        summarize(reviews)
+        kwargs = openai_mock.call_args.kwargs
+        expected = {"api_key": None}
+        self.assertEqual(kwargs, expected)
+
+    @patch("src.summarize.summarize.OpenAI")
+    def test_summarize_under_4000_tokens(self, openai_mock):
+        reviews = ["two words"] * 5000
         summarize(reviews)
         kwargs = openai_mock.call_args.kwargs
         expected = {"api_key": None}
