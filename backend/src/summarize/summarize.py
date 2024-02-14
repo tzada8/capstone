5bfc86b10d59fd0b68c858ed7f79789428c7c878
Troy Zada <57775285+tzada8@users.noreply.github.com>
2024-02-14 00:38:25 -0500

    Add token limitation (#32)

--- backend/src/summarize/summarize.py
+++ backend/src/summarize/summarize.py
@@ -6,10 +6,19 @@ def summarize(reviews: List[str]) -> str:
     if len(reviews) == 0:
         return "Product has no reviews."
 
-    # TODO: Need to limit to under 4097 tokens.
+    # Limit to under 4000 tokens.
+    shorted_reviews = []
+    max_tokens = 4000 # Actual max is 4098, but would prefer some buffer.
+    total_tokens = 0
+    for review in reviews:
+        tokens = len(review.split())
+        if total_tokens + tokens <= max_tokens:
+            shorted_reviews.append(review)
+            total_tokens += tokens
+
     client = OpenAI(api_key=environ.get("OPENAI_API_KEY"))
     # TODO: Maybe can add more to prompt (e.g. "using at most 100 words").
-    bullet_reviews = "\n- ".join(reviews)
+    bullet_reviews = "\n- ".join(shorted_reviews)
     prompt = f"Summarize and aggregate the following list of product reviews into 1 paragraph:\n- {bullet_reviews}"
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
