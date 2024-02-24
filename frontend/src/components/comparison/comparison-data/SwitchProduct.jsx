a0cd2d6bf78ccaff0b2479e7b0130ddc35f33057
Troy Zada <tzada@uwaterloo.ca>
2024-02-24 01:35:19 -0500

    Added product switch styling

--- frontend/src/components/comparison/comparison-data/SwitchProduct.jsx
+++ frontend/src/components/comparison/comparison-data/SwitchProduct.jsx
@@ -4,9 +4,9 @@ import "./ComparisonData.css";
 
 function SwitchProduct(props) {
 	return (
-		<div>
+		<div className="product-switch-container">
             <select
-                className="body-1"
+                className="body-1 product-switch-dropdown"
                 id={`switch-product-${props.i}`}
                 name={`switch-product-${props.i}`}
                 value={props.selectedTitle}
