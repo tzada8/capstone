3b82d24c52d3d50c0df8b2ba5552beb1e0dfac0f
Troy Zada <57775285+tzada8@users.noreply.github.com>
2024-02-28 22:26:38 -0500

    Drag and drop form (#54)
    * Moved title and description to general Modal component
    
    * Extracted form modal functionality to separate modals
    
    * Add hover functionality to preferences form
    
    * Add drag-and-drop functionality to feature priority form
    
    * Add Product Rating as ranking option

--- frontend/src/components/modals/content/feature-priority/FeaturePriorityModal.jsx
+++ frontend/src/components/modals/content/feature-priority/FeaturePriorityModal.jsx
@@ -1,53 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { Button } from "antd";
 
-import FormModal from "../../form-modal/FormModal";
+import "./FeaturePriorityModal.css";
+import Modal from "../../base-modal/Modal";
 
 function FeaturePriorityModal({ onSubmit, isOpen, onClose }) {
     // TODO: Maybe have list of features dependant on preferences to backend (allows for dynamic set of products/options).
-    const featurePriorityOptions = [
-        { value: "", display: "Please select" },
+    const [featurePriority, setFeaturePriority] = useState([
         { value: "brand", display: "Brand" },
         { value: "megapixels", display: "Number of megapixels" },
         { value: "lens_type", display: "Lens type" },
         { value: "camera_type", display: "Camera type" },
         { value: "budget", display: "Budget" },
-    ]
-    
-    const featurePriorityForm = {
-        "1": { label: "1.", initial: "", type: "select", options: featurePriorityOptions },
-        "2": { label: "2.", initial: "", type: "select", options: featurePriorityOptions },
-        "3": { label: "3.", initial: "", type: "select", options: featurePriorityOptions },
-        "4": { label: "4.", initial: "", type: "select", options: featurePriorityOptions },
-        "5": { label: "5.", initial: "", type: "select", options: featurePriorityOptions },
-    }
-    const [duplicateErrorMessage, setDuplicateErrorMessage] = useState("");
+        { value: "product_rating", display: "Product Rating" },
+    ]);
+    const dragFeature = useRef(0);
+    const draggedOverFeature = useRef(0);
 
-    const submitPriority = (formState) => {
-        const ignoreEmptyStrings = Object.fromEntries(
-            Object.entries(formState).filter(([_, value]) => value !== "")
-        );
-        const formKeys = Object.keys(ignoreEmptyStrings);
-        const uniqueFormValues = new Set(formKeys.map(k => formState[k]));
-        if (uniqueFormValues.size === formKeys.length) {
-            onSubmit(formState);
-        } else {
-            setDuplicateErrorMessage("Please select a unique set of features");
-            setTimeout(() => {
-                setDuplicateErrorMessage("");
-            }, 5000);
-        }
+    const handleSort = () => {
+        const featurePriorityClone = [...featurePriority];
+        const temp = featurePriorityClone[dragFeature.current];
+        featurePriorityClone[dragFeature.current] = featurePriorityClone[draggedOverFeature.current];
+        featurePriorityClone[draggedOverFeature.current] = temp;
+        setFeaturePriority(featurePriorityClone);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formSubmit = {};
+        featurePriority.forEach((fp, i) => formSubmit[String(i + 1)] = fp.value);
+        onSubmit(formSubmit);
+    }
+
+    const handleSkip = (event) => {
+        event.preventDefault();
+        const formSkip = {};
+        featurePriority.forEach((_, i) => formSkip[String(i + 1)] = "");
+        onSubmit(formSkip);
     }
 
 	return (
-        <FormModal
-            formTitle="Priority of features"
-            formDescription="Rank the features from highest to least importance to you"
-            errorMessage={duplicateErrorMessage}
-            formQuestions={featurePriorityForm}
-            onSubmit={submitPriority}
+        <Modal
+            title="Priority of features"
+            description="Rank the features from highest to least importance to you"
+            onSubmit={handleSubmit}
             isOpen={isOpen}
             onClose={onClose}
-        />
+        >
+            <form onSubmit={handleSubmit}>
+                {featurePriority.map((feature, index) => (
+                    <div className="form-row-select">
+                        <p className="body-1-medium select-label-question">{String(index + 1)}.</p>
+                        <div
+                            className="select-label"
+                            draggable
+                            onDragStart={() => dragFeature.current = index}
+                            onDragEnter={() => draggedOverFeature.current = index}
+                            onDragEnd={handleSort}
+                            onDragOver={(e) => e.preventDefault()}
+                        >
+                            <p className="body-1">{feature.display}</p>
+                        </div>
+                    </div>
+                ))}
+                <Button htmlType="submit" type="primary" size="large" block className="primary-button form-button-spacing form-button-size">Next</Button>
+                <Button onClick={handleSkip} type="primary" size="large" block ghost className="primary-button form-button-size">Skip</Button>
+            </form>
+        </Modal>
 	);
 }
 
