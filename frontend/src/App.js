e6bd87ab53e5b4b825b4914ab9522d1a4af15e6c
Troy Zada <57775285+tzada8@users.noreply.github.com>
2024-03-06 01:19:25 -0500

    Resizing bugs (#59)
    * Ensure products containers are always same size
    
    * Resolve useEffect dependency warnings
    
    * Ensure pages starts from the top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,16 +9,18 @@ import { routes } from "./routes/Routes";
 import Comparisons from "./pages/comparisons/Comparisons";
 import Home from "./pages/home/Home";
 import ProductSearch from "./pages/product-search/ProductSearch";
+import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path={routes.home} element={<Home />} />
-          <Route path={routes.productSearch} element={<ProductSearch />} />
-          <Route path={routes.comparisons} element={<Comparisons />} />
-        </Routes>
+        <ScrollToTop />
+          <Routes>
+            <Route path={routes.home} element={<Home />} />
+            <Route path={routes.productSearch} element={<ProductSearch />} />
+            <Route path={routes.comparisons} element={<Comparisons />} />
+          </Routes>
       </BrowserRouter>
     </div>
   );
