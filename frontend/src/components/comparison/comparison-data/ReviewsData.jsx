32272f6845ca825d787bb249a3cab22d9b191967
Troy Zada <tzada@uwaterloo.ca>
2024-03-08 12:30:12 -0500

    Implement expert reviews

--- frontend/src/components/comparison/comparison-data/ReviewsData.jsx
+++ frontend/src/components/comparison/comparison-data/ReviewsData.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./ComparisonData.css";
+import consumerReportsIcon from "../../../images/comparisons/consumer-reports-icon.png";
 import MostReview from "./MostReview";
 
 function ReviewsData(props) {
@@ -23,7 +24,16 @@ function ReviewsData(props) {
                 title={props.reviews.top_negative?.title}
                 text={props.reviews.top_negative?.text}
             />}
-            {/* TODO: Add expert reviews. */}
+            <br/>
+            <br/>
+            {props.reviews.expert_review && <div className="expert-reviews-container">
+                <div className="expert-reviews-title-position">
+                    <img className="expert-reviews-icon" src={consumerReportsIcon} alt=""/>
+                    <h4 className="expert-reviews-source">{props.reviews.expert_review?.source}</h4>
+                </div>
+                <p className="body-3 expert-reviews-score">{props.reviews.expert_review?.score}</p>
+                <p className="body-1">{props.reviews.expert_review?.review}</p>
+            </div>}
         </div>
 	);
 }
