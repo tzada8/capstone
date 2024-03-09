31ad4d05b31ce66ab05dc9432a3fe72a8526fea8
Troy Zada <tzada@uwaterloo.ca>
2024-03-09 14:21:40 -0500

    Added more and less buttons to reviews text

--- frontend/src/components/comparison/comparison-data/MostReview.jsx
+++ frontend/src/components/comparison/comparison-data/MostReview.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ComparisonData.css";
 import Rating from "../../rating/Rating";
 
 function MostReview(props) {
+    const wordLimit = 50;
+    const reviewText = props.text || "";
+    const [showMore, setShowMore] = useState(false);
+
+    const numWords = reviewText.split(" ").length;
+    const shortenedText = reviewText.split(" ").slice(0, wordLimit).join(" ");
+    const displayableText = numWords > wordLimit ? shortenedText + "..." : shortenedText;
+    const buttonText = numWords > wordLimit ? (showMore ? "Less" : "More") : "";
+
     const reviewType = props.isPositive ? "POSITIVE" : "NEGATIVE";
     const reviewClass = props.isPositive ? "positive-review-title" : "negative-review-title"
 
@@ -13,7 +22,9 @@ function MostReview(props) {
             <div className="ratings-container">
                 <Rating rating={props.rating} reviews={null} isCenter={false} />
             </div>
-            <p className="body-1">{props.text}</p>
+            <p className="body-1">
+                {showMore ? reviewText : displayableText} <button className="show-more-text" onClick={() => setShowMore(!showMore)}>{buttonText}</button>
+            </p>
         </div>
 	);
 }
