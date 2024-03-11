611321cf3d3c712856b377004623a7c6912e5f24
Troy Zada <57775285+tzada8@users.noreply.github.com>
2024-03-10 20:11:25 -0400

    Update priority form designs (#61)
    * Add multiple modal sizes
    
    * Text updates
    
    * Add drag icons
    
    * Priority ranking is no longer swap functionality

--- frontend/src/components/modals/content/feature-priority/FeaturePriorityModal.jsx
+++ frontend/src/components/modals/content/feature-priority/FeaturePriorityModal.jsx
@@ -11,16 +11,15 @@ function FeaturePriorityModal({ onSubmit, isOpen, onClose }) {
         { value: "lens_type", display: "Lens type" },
         { value: "camera_type", display: "Camera type" },
         { value: "budget", display: "Budget" },
-        { value: "product_rating", display: "Product Rating" },
+        { value: "product_rating", display: "Peer reviews and ratings" },
     ]);
     const dragFeature = useRef(0);
     const draggedOverFeature = useRef(0);
 
     const handleSort = () => {
         const featurePriorityClone = [...featurePriority];
-        const temp = featurePriorityClone[dragFeature.current];
-        featurePriorityClone[dragFeature.current] = featurePriorityClone[draggedOverFeature.current];
-        featurePriorityClone[draggedOverFeature.current] = temp;
+        const movedFeature = featurePriorityClone.splice(dragFeature.current, 1)[0];
+        featurePriorityClone.splice(draggedOverFeature.current, 0, movedFeature);
         setFeaturePriority(featurePriorityClone);
     }
 
@@ -41,7 +40,7 @@ function FeaturePriorityModal({ onSubmit, isOpen, onClose }) {
 	return (
         <Modal
             title="Priority of features"
-            description="Rank the features from highest to least importance to you"
+            description="Drag and drop to rank features from highest to lowest importance to you"
             onSubmit={handleSubmit}
             isOpen={isOpen}
             onClose={onClose}
@@ -49,16 +48,26 @@ function FeaturePriorityModal({ onSubmit, isOpen, onClose }) {
             <form onSubmit={handleSubmit}>
                 {featurePriority.map((feature, index) => (
                     <div className="form-row-select">
-                        <p className="body-1-medium select-label-question">{String(index + 1)}.</p>
+                        <p className="body-1-medium select-label-question">{String(index + 1)}</p>
                         <div
-                            className="select-label"
+                            className="draggable-option"
                             draggable
                             onDragStart={() => dragFeature.current = index}
                             onDragEnter={() => draggedOverFeature.current = index}
                             onDragEnd={handleSort}
                             onDragOver={(e) => e.preventDefault()}
                         >
-                            <p className="body-1">{feature.display}</p>
+                            <div className="select-label body-1">{feature.display}</div>
+                            <div className="drag-outline-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                                    <path
+                                        fill="currentColor"
+                                        fill-rule="evenodd"
+                                        d="M19 10a4 4 0 1 1-8 0a4 4 0 0 1 8 0m-4 18a4 4 0 1 0 0-8a4 4 0 0 0 0 8m0 14a4 4 0 1 0 0-8a4 4 0 0 0 0 8m22-32a4 4 0 1 1-8 0a4 4 0 0 1 8 0m-4 18a4 4 0 1 0 0-8a4 4 0 0 0 0 8m0 14a4 4 0 1 0 0-8a4 4 0 0 0 0 8"
+                                        clip-rule="evenodd"
+                                    />
+                                </svg>
+                            </div>
                         </div>
                     </div>
                 ))}
