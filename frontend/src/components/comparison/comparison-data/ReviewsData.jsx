aefdcea2c68a79054e334632364904ff40488121
Troy Zada <57775285+tzada8@users.noreply.github.com>
2024-03-10 20:11:45 -0400

    Added more and less buttons to reviews text (#66)

--- frontend/src/components/comparison/comparison-data/ReviewsData.jsx
+++ frontend/src/components/comparison/comparison-data/ReviewsData.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ComparisonData.css";
 import consumerReportsIcon from "../../../images/comparisons/consumer-reports-icon.png";
 import MostReview from "./MostReview";
 
 function ReviewsData(props) {
+    const wordLimit = 50;
+    const expertReviewText = props.reviews.expert_review?.review || "";
+    const [showMore, setShowMore] = useState(false);
+
+    const numWords = expertReviewText.split(" ").length;
+    const shortenedText = expertReviewText.split(" ").slice(0, wordLimit).join(" ");
+    const displayableText = numWords > wordLimit ? shortenedText + "..." : shortenedText;
+    const buttonText = numWords > wordLimit ? (showMore ? "Less" : "More") : "";
+
 	return (
 		<div>
             {props.reviews.summary && <p className="body-1">{props.reviews.summary}</p>}
@@ -32,7 +41,9 @@ function ReviewsData(props) {
                     <h4 className="expert-reviews-source">{props.reviews.expert_review?.source}</h4>
                 </div>
                 <p className="body-3 expert-reviews-score">{props.reviews.expert_review?.score}</p>
-                <p className="body-1">{props.reviews.expert_review?.review}</p>
+                <p className="body-1">
+                    {showMore ? expertReviewText : displayableText} <button className="show-more-text" onClick={() => setShowMore(!showMore)}>{buttonText}</button>
+                </p>
             </div>}
         </div>
 	);
